b10cc1715c99bbc8c662962b54291773a30a1fcc
Safe_Mode <safe2mode@gmail.com>
2023-02-25 14:26:54 +0400

    Implement log level definition

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,6 +1,16 @@
 import { ConsoleLogger } from '@nestjs/common';
+import { LogLevel } from '@nestjs/common/services/logger.service';
+import { env } from 'node:process';
+import { LOG_LEVELS, MAX_LOG_LEVEL } from 'src/app.const';
 
 export class LoggingService extends ConsoleLogger {
+  constructor(context?: string) {
+    super(context);
+    
+    this.setLogLevels(
+      LoggingService.getLogLevel(env.LOG_LEVEL)
+    );
+  }
   /**
    * Write a 'log' level log.
    */
@@ -42,5 +52,13 @@ export class LoggingService extends ConsoleLogger {
             `${result}${param}: ${object[param]}${Object.keys(object).length - 1 === index ? ' }' : ', '}`,
         '{ '
     );
+  }
+
+  static getLogLevel(level: string): LogLevel[] {
+    if (+level > MAX_LOG_LEVEL) {
+      throw new Error('Invalid log level argument');
+    }
+
+    return LOG_LEVELS.slice(0, +level);
   }
 }
